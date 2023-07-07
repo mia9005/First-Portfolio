2e915e90a7f70b9dada0a2a1f0ee62322a13ff73
Jeongeun Lee <jungeun9005@gmail.com>
2023-07-06 18:02:52 -0700

    remove some file

--- src/main.js
+++ src/main.js
@@ -1,5 +1,4 @@
 import { createApp } from 'vue'
 import App from './App.vue'
-import router from './components/router/router';
 
-createApp(App).use(router).mount('#app')
+createApp(App).mount('#app')
